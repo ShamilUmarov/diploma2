3550c97262cf472a40fe4a944daef8ae73c796ca
ShamilUmarov <umarovsamil209@gmail.com>
2022-06-29 22:28:58 +0600

    add commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,14 +8,14 @@ function Home() {
   return (
     <>
       <Header
-        title="Burger 
+        title="Sweet
    Bachelor"
         image={headerImage}
       ></Header>
 
       <div class="section-title ">
-        <span>Burger Menu</span>
-        <h3>Best Ever Burgers</h3>
+        <span>Sweet Menu</span>
+        <h3>The best sweets</h3>
       </div>
       <ProductList products={getProducts("Burgers")} />
       <Link class="boxed-btn5" to={"/products/"}>
