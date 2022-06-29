c8fdc60c4cbe2eab091ce09b395c91e1c583928d
ShamilUmarov <umarovsamil209@gmail.com>
2022-06-29 14:34:07 +0600

    Delete errors

--- src/components/CartButton/CartButton.js
+++ src/components/CartButton/CartButton.js
@@ -9,7 +9,7 @@ export default function CartButton({ productId }) {
   }
 
   return (
-    <button className="CartButton" onClick={onAddToCart}>Add to card</button>
+    <button className="CartButton" onClick={onAddToCart}>Add to cards</button>
     
   );
 }
