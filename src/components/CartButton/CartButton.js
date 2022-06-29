94365467cdc6de8eed9a28489a56af14d99829f5
ShamilUmarov <umarovsamil209@gmail.com>
2022-06-29 23:07:03 +0600

    Add new comment

--- src/components/CartButton/CartButton.js
+++ src/components/CartButton/CartButton.js
@@ -9,7 +9,7 @@ export default function CartButton({ productId }) {
   }
 
   return (
-    <button className="CartButton" onClick={onAddToCart}>Add to</button>
+    <button className="CartButton" onClick={onAddToCart}>Add t</button>
     
   );
 }
